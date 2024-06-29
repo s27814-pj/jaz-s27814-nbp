b46f52826ca07ced504b298fa5a406ebf5d041c2
s27814-pj <s27814@pjwstk.edu.pl>
2024-06-29 10:09:29 +0200

    init commit

---
+++ src/main/java/com/example/demo/JazS27814NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27814NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS27814NbpApplication.class, args);
+	}
+
+}
